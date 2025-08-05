ccd214c00836d22cecd7e2cc1ca6a67a30f02470
hrafael5 <hrafael5@gmail.com>
2025-08-05 16:46:45 -0300

    Deploy autom├ítico

--- deploy.bat
+++ deploy.bat
@@ -10,7 +10,7 @@ echo ==========================================================
 echo [2/3] Conectando na VPS e executando os comandos...
 
 ssh -tt root@82.29.59.139 ^
-"su - arbflash -l -c 'cd arbitral_bot && git pull && npm install && /home/arbflash/.nvm/versions/node/v22.17.0/bin/pm2 restart arbflash-bot && read -p \"Pressione ENTER para sair...\" </dev/tty'"
+"su - arbflash -l -c \"cd arbitral_bot && git pull && npm install && /home/arbflash/.nvm/versions/node/v22.17.0/bin/pm2 restart arbflash-bot && read -p 'Pressione ENTER para sair...' < /dev/tty\""
 
 echo.
 echo ==========================================================
