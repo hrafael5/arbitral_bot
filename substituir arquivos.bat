148f31e5bfc8b018733786ebee6ecd0dc158e2d9
hrafael5 <hrafael5@gmail.com>
2025-08-05 15:36:29 -0300

    Deploy autom├ítico

--- substituir arquivos.bat
+++ substituir arquivos.bat
@@ -7,7 +7,7 @@ git push
 echo Conectando na VPS e atualizando o projeto remoto...
 
 ssh root@82.29.59.139 ^
-"su - arbflash -c 'cd arbitral_bot && git pull && npm install && pm2 restart arbflash-bot'"
+"su - arbflash -c \"cd arbitral_bot && git pull && npm install && pm2 restart arbflash-bot\""
 
 echo ---
 echo Deploy finalizado com sucesso!
