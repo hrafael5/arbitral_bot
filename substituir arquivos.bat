700f52423176a05577eb43ac362cb5da87a70d1a
hrafael5 <hrafael5@gmail.com>
2025-08-05 15:29:56 -0300

    Deploy autom├ítico

---
+++ substituir arquivos.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Enviando atualizações para o GitHub...
+git add .
+git commit -m "Deploy automático"
+git push
+
+echo Conectando na VPS e atualizando o projeto remoto...
+
+ssh root@82.29.59.139 ^
+"su - arbflash -c 'cd arbitral_bot && git pull && npm install && pm2 restart arbflash-bot'"
+
+echo ---
+echo Deploy finalizado com sucesso!
+pause
